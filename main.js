98f278eefb9f304d4ede9016d70b26565d0101ea
SRISH2022 <108571212+SRISH2022@users.noreply.github.com>
2022-08-13 18:55:13 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+objects = [];
+status = "";
+video = "";
+
+function preload() 
+{
+video = createVideo('video.mp4');
+video.hide;
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if (status != "") 
+    {
+        objectDetector.detect(video, gotReult)   
+        for (i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Nember of objects detected are : " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+}
+function start() 
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(result);
+    objects= results;
+}
